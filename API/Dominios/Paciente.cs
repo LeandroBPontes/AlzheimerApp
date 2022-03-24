1d6433eb802fef3ba68c5633446181e335b24fb2
Leandro Barros Pontes <65022896+LeandroBPontes@users.noreply.github.com>
2022-03-24 08:39:49 -0300

    Update Paciente.cs

--- API/Dominios/Paciente.cs
+++ API/Dominios/Paciente.cs
@@ -4,6 +4,9 @@ namespace AlzheimerApp.Dominios {
         public bool PossuiPlano { get; set; }
         public Cuidador Cuidador { get; set; }
         public int IdCuidador{ get; set; }
+        public int IdMedicamento{ get; set; }
+        public int IdAtividade{ get; set; }
+        public int IdSintoma{ get; set; }
 
     }
 }
