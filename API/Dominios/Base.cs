834c86a65a44c7feb3242cee6efe921bc23d3831
Leandro Barros Pontes <65022896+LeandroBPontes@users.noreply.github.com>
2022-03-24 09:53:30 -0300

    ajustes estruturais

--- API/Dominios/Base.cs
+++ API/Dominios/Base.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace AlzheimerApp.Dominios {
+    //medicamento, atividade e sintoma possuem o id do paciente
+    //logo poderemos em um unico paciente, ter varios destes
     public class Base {
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Horario { get; set; }
         public string Frequencia { get; set; }
+        public string IdPaciente { get; set; }
     }
 }
--- API/Dominios/Paciente.cs
+++ API/Dominios/Paciente.cs
@@ -3,10 +3,7 @@ namespace AlzheimerApp.Dominios {
     public class Paciente : PacienteCuidador {
         public bool PossuiPlano { get; set; }
         public Cuidador Cuidador { get; set; }
-        public int IdCuidador{ get; set; }
-        public int IdMedicamento{ get; set; }
-        public int IdAtividade{ get; set; }
-        public int IdSintoma{ get; set; }
+        public int IdCuidador { get; set; }
 
     }
 }
